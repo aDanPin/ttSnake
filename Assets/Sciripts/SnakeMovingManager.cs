98d44215b29c527cf949f274a483ce25c6dbaa0e
Pinaev Danil <drobrorobro@gmail.com>
2021-09-13 12:18:12 +0300

    Make first prefabs and snake

---
+++ Assets/Sciripts/SnakeMovingManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovingManager : MonoBehaviour
+{
+    public Vector3 mainMovingDirection;
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveUpToMainDirection();        
+    }
+
+    private void MoveUpToMainDirection() {
+        transform.position += mainMovingDirection.normalized
+                                * speed * Time.deltaTime;
+    }
+}
